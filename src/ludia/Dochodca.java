d8458f605f2e8079cb2c3991bd87679ae0a05135
suranjakub <xsuranj@stuba.sk>
2020-03-31 23:07:59 +0200

    added features in Dochodca

--- src/ludia/Dochodca.java
+++ src/ludia/Dochodca.java
@@ -3,6 +3,18 @@ package ludia;
 public class Dochodca {
     private int x, y, id;
     private String meno;
+    private static int pocDochodcov = 0;
+
+    public Dochodca() {
+        this.meno = "Jozko";
+        this.x = getRandomNumberInRange(1, 200);
+        this.y = getRandomNumberInRange(1, 200);
+        this.id = ++pocDochodcov;
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+        return (int)(Math.random() * ((max - min) + 1)) + min;
+    }
 
     public void setName(String meno) {
         this.meno = meno;
