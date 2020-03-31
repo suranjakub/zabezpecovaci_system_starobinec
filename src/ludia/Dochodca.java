42444a3eeca45531fc8b02bf5043c1b10a21539a
suranjakub <xsuranj@stuba.sk>
2020-03-31 22:06:50 +0200

    added class dochodca in ludia

--- src/ludia/Dochodca.java
+++ src/ludia/Dochodca.java
@@ -1,4 +1,14 @@
 package ludia;
 
 public class Dochodca {
+    private int x, y, id;
+    private String meno;
+
+    public void setName(String meno) {
+        this.meno = meno;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 }
