1864024eec079abd334524cfcaf8d09ee7c51765
suranjakub <xsuranj@stuba.sk>
2020-03-31 21:35:34 +0200

    edit main to show StarobinecGUI

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import gui.StarobinecGUI;
+import javafx.application.Application;
 
 public class Main {
+    public static void main(String[] args) {
+        Application.launch(StarobinecGUI.class, args);
+    }
 }
---
+++ src/gui/StarobinecGUI.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StarobinecGUI extends Application {
+    @Override
+    public void start(Stage hlavneOkno) throws Exception {
+        hlavneOkno.setTitle("Starobinec");
+        hlavneOkno.show();
+    }
+}
