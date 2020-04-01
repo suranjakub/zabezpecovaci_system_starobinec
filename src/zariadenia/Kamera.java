d00d44c363a33bab247923a1a5d17d21999387f6
suranjakub <xsuranj@stuba.sk>
2020-04-01 12:48:02 +0200

    added base structure of Kamera

--- src/zariadenia/Kamera.java
+++ src/zariadenia/Kamera.java
@@ -1,4 +1,14 @@
 package zariadenia;
 
-public class Kamera {
+public class Kamera extends Zariadenie {
+    public void skontrolujDochodcov() {
+        if(jeVdb()) {
+
+        }
+        //nie je v databaze
+        else {
+            ukazRecepcnemu();
+            ulozitDoDB();
+        }
+    }
 }
